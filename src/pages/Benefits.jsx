eebe3a1866812292edb9b436985f76c60c347031
Booklover997 <65503002+Booklover997@users.noreply.github.com>
2024-05-20 16:45:30 -0400

    Changed some font sizes

--- src/pages/Benefits.jsx
+++ src/pages/Benefits.jsx
@@ -9,7 +9,7 @@ export default function Benefits() {
       <Nav />
       <div class="taxContainer">
         <div class="textSection">
-    <h1 class="taxTitle">Save Up to $3,200 with tax Rebates</h1>
+    <h1 class="taxTitle">Save Up to $3,200</h1>
     <p>By investing in renewable technologies for your home you are able to qualify for federal tax credits such as the eneergy efficiency home improvement credit and the residential clean energy credit. Some examples of qualifying technologies are wind, solar, geothermal, fuel cells, and battery storage. These credits available through 2032 allower homeowners to claim up to $3,200 in tax rebates annually. These credits can be claimed from the IRS by using <a href="https://www.irs.gov/pub/irs-pdf/f5695.pdf" class="link">IRS Form 5965</a> 
 </p>
 </div> 
